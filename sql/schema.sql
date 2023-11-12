063da899c88b65c49e3addc941d2bd0d33c8ebb1
kingcollins17 <98811110+kingcollins17@users.noreply.github.com>
2023-11-12 17:27:24 +0100

    Modified the schema.sql again

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS `Quiz`;
-CREATE DATABASE `Quiz` DEFAULT CHARACTER
+DROP DATABASE IF EXISTS `quiz`;
+CREATE DATABASE `quiz` DEFAULT CHARACTER
 SET
      utf8 COLLATE utf8_unicode_ci;
-USE Quiz;
+USE quiz;
 CREATE TABLE
-     IF NOT EXISTS `Users` (
+     IF NOT EXISTS `users` (
           `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
           `user_name` VARCHAR(50),
           `email` VARCHAR(100) NOT NULL UNIQUE,
@@ -13,22 +13,22 @@ CREATE TABLE
           PRIMARY KEY `user_id` (`id`)
      ) ENGINE = InnoDB;
 CREATE TABLE
-     IF NOT EXISTS `Categories` (
+     IF NOT EXISTS `categories` (
           `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
           `name` VARCHAR(255) NOT NULL UNIQUE,
           PRIMARY KEY (`id`)
      ) ENGINE = InnoDB;
 CREATE TABLE
-     IF NOT EXISTS `Topics` (
+     IF NOT EXISTS `topics` (
           `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
           `title` VARCHAR(100) NOT NULL UNIQUE,
           `description` VARCHAR (1024) NOT NULL DEFAULT "",
           `category_id` INT UNSIGNED NOT NULL,
           PRIMARY KEY (`id`),
-          CONSTRAINT `fk_topics_cat` FOREIGN KEY (`category_id`) REFERENCES `Quiz`.`Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+          CONSTRAINT `fk_topics_cat` FOREIGN KEY (`category_id`) REFERENCES `quiz`.`categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
      ) ENGINE = InnoDB;
 CREATE TABLE
-     IF NOT EXISTS `Questions` (
+     IF NOT EXISTS `questions` (
           `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
           `question` VARCHAR(512) NOT NULL UNIQUE,
           `A` VARCHAR(100) NOT NULL,
@@ -40,22 +40,22 @@ CREATE TABLE
           `topic_id` INT UNSIGNED NOT NULL,
           `user_id` INT UNSIGNED NOT NULL,
           PRIMARY KEY (`id`),
-          CONSTRAINT `fk_questions_topic` FOREIGN KEY (`topic_id`) REFERENCES `Quiz`.`Topics` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-          FOREIGN KEY (`user_id`) REFERENCES `Quiz`.`Users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+          CONSTRAINT `fk_questions_topic` FOREIGN KEY (`topic_id`) REFERENCES `quiz`.`topics` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+          FOREIGN KEY (`user_id`) REFERENCES `quiz`.`users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
      ) ENGINE = InnoDB;
 CREATE TABLE
-     IF NOT EXISTS `Quiz` (
+     IF NOT EXISTS `quiz` (
           `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
           `title` VARCHAR(100) NOT NULL UNIQUE,
           `questions` VARCHAR(2048) NOT NULL,
           `user_id` INT UNSIGNED NOT NULL,
           `topic_id` INT UNSIGNED NOT NULL,
           `duration` INT UNSIGNED NOT NULL DEFAULT '1800',
-          FOREIGN KEY (`topic_id`) REFERENCES `Quiz`.`Topics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-          PRIMARY KEY `Quiz_id` (`id`),
-          CONSTRAINT `fk_Quiz_users`
+          FOREIGN KEY (`topic_id`) REFERENCES `quiz`.`topics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+          PRIMARY KEY `quiz_id` (`id`),
+          CONSTRAINT `fk_quiz_users`
             FOREIGN KEY (`user_id`)
-            REFERENCES `Quiz`.`users` (`id`)
+            REFERENCES `quiz`.`users` (`id`)
             ON DELETE NO ACTION
             ON UPDATE CASCADE
 ) ENGINE = InnoDB;
